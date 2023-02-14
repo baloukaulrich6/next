e5b8a83717290aa6230f77889fd3b48893483f05
teddy <baloukaulrich64@gmail.com>
2023-02-15 00:40:45 +0100

    cart 2

---
+++ pages/api/product/[id].js
@@ -0,0 +1,36 @@
+import Product from '../../../models/Product'
+import nc from 'next-connect'
+import db from "../../../utils/db"
+const handler = nc()
+
+handler.get(async (req, res) =>{
+    try{
+        db.connectDb()
+        const id = req.query.id;
+        const style = req.query.style || 0
+        const size = req.query.size || 0
+        const product = await Product.findById(id).lean();
+        let discount = product.subProducts[style].discount;
+        let priceBefore = product.subProducts[style].sizes[size].price;
+        let price = discount ? priceBefore - priceBefore / discount : priceBefore;
+        db.disconnectDb()
+        return res.json({
+            _id : product._id,
+            style: Number(style),
+            name: product.name,
+            description: product.description,
+            slug: product.slug,
+            sku: product.subProducts[style].sku,
+            brand: product.brand,
+            shipping: product.brand,
+            images: product.subProducts[style].images,
+            color: product.subProducts[style].color,
+            price,
+            priceBefore,
+            quantity: product.subProducts[style].sizes[size].qty
+        })
+    }catch(err){
+        return res.status(500).json({message: err.message})
+    }
+})
+export default handler

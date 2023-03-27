e636d51ecbd38c9ab6289f1f305e27d41528d012
teddy <baloukaulrich64@gmail.com>
2023-03-27 12:12:07 +0200

    categories

---
+++ components/admin/categories/Create.js
@@ -0,0 +1,54 @@
+import { Form, Formik } from "formik"
+import { useState } from "react"
+import styles from "./styles.module.scss"
+import * as Yup from 'yup';
+import AdminInput from "../../inputs/adminInput";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export default function Create({setCategories}) {
+    const [name, setName] = useState("");
+    const validate = Yup.object({
+        name: Yup.string().required('Category name is required')
+        .min(3, "Category name must be between  3 and 20 character.")
+        .max(20, "Category name must be between  3 and 20 character.")
+        .matches(
+            /^[a-zA-Z\s]*$/,
+            "Numbers and special charcters are not allowed."),
+    })
+    const submitHandler = async() =>{
+        try{
+            const {data} = await axios.post("/api/admin/category", {name})
+            setCategories(data.categories)
+            setName('')
+            toast.success(data.message)
+        }catch(error){
+            toast.error(error.response.data.message)
+        }
+    }
+  return (
+    <>
+     <Formik
+     enableReinitialize
+     initialValues={{name}}
+     validationSchema={validate}
+     onSubmit={()=>{submitHandler()}}>
+        {(formik) =>(
+            <Form>
+                <div className={styles.header}>create a Category</div>
+                <AdminInput
+                type= "text"
+                label="Name"
+                name='name'
+                placeholder="Category name"
+                onChange={(e) => setName(e.target.value)}
+                />
+                <button type="submit" className={`${styles.btn}`}>
+                    <span>Add Category</span>
+                </button>
+            </Form>
+        )}    
+    </Formik>   
+    </>
+  )
+}
---
+++ pages/api/admin/category.js
@@ -0,0 +1,55 @@
+import nc from 'next-connect'
+import slugify from 'slugify';
+import auth from '../../../middleware/auth'
+import Category from '../../../models/Category';
+import db from '../../../utils/db';
+const handler = nc().use(auth)
+
+handler.post(async(req, res) =>{
+    try{
+        const {name } = req.body;
+        db.connectDb()
+        const test = await Category.findOne({name})
+        if(test){
+             return res
+             .status(400).json({message: "Category already exist, Try a different name"})
+        }
+        await new Category({name, slug: slugify(name)}).save();
+        db.disconnectDb()
+        res.json({
+            message: `Category ${name} has been created Successfully.`,
+            categories: await Category.find({}).sort({ updatedAt: -1 })
+        })
+    }catch(err){
+        db.disconnectDb()
+        res.status(500).json({message: err.message})    }
+})
+handler.delete(async(req, res) =>{
+    try{
+        const {id} = req.body;
+        db.connectDb();
+        await Category.findByIdAndRemove(id)
+        db.disconnectDb();
+        return res.json({
+            message: 'Category has been deleted successfuly',
+            categories: await Category.find({}).sort({updatedAt: -1 })
+        })
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+handler.put(async(req, res) =>{
+    try{
+        const {id, name} = req.body;
+        db.connectDb();
+        await Category.findByIdAndUpdate(id, {name})
+        db.disconnectDb();
+        return res.json({
+            message: 'Category has been updated successfuly',
+            categories: await Category.find({}).sort({createdAt: -1 })
+        })
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+export default handler

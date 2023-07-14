2966ea7a5a5770d71550ce38b629d5a83eb28544
teddy <baloukaulrich64@gmail.com>
2023-07-14 17:45:17 +0200

    product

--- components/productCard/index.js
+++ components/productCard/index.js
@@ -33,10 +33,11 @@ export default function ProductCard({ product }) {
         })
     );
   }, [active, product]);
+
   return (
     <div className={styles.product}>
       <div className={styles.product__container}>
-      <a href={`/product/${product.slug}?style=${active}`} target="_blank" rel="noopener noreferrer">
+      <a href={`product/${product.slug}?style=${active}`} target="_blank" rel="noopener noreferrer">
           <div>
             <ProductSwiper images={images} />
           </div>
